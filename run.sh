c643d46bc74a454ce878d56d9f3ca2c714cc4613
31-ryougishiki <3154764788@qq.com>
2026-09-14 09:01:14 +0800

    配置ASCEND_RT_VISIBLE_DEVICES适配tp_size=16

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ export OMP_PROC_BIND=false
 export OMP_NUM_THREADS=10
 export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 export TASK_QUEUE_ENABLE=1
 
 # 缓存禁用
